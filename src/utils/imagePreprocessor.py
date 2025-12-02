99bb36de74073777e391c35dae803b73dd38a643
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2025-12-02 16:44:12 +0100

    tweak(imagePreprocessor) add transformations to increase artificially dataset

--- src/utils/imagePreprocessor.py
+++ src/utils/imagePreprocessor.py
@@ -13,7 +13,11 @@ def preprocessImagesDataset(dataSetPath):
 
     preprocess = transforms.Compose([
         transforms.Resize(256),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         transforms.CenterCrop(224),
+
         transforms.ToTensor(),
     ])
 
