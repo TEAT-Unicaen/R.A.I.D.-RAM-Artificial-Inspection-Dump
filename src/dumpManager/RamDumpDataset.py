849314bfe3f25ed750f2827f664c1738f22b269b
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2026-01-29 19:24:19 +0100

    tweak(RamDumpDataset) rework to allow multi threading

--- src/dumpManager/RamDumpDataset.py
+++ src/dumpManager/RamDumpDataset.py
@@ -3,66 +3,43 @@ import torch
 from torch.utils.data import Dataset
 import mmap
 
-def mmap_file(f):
-    """
-    Reads a file using memory-mapping for efficient access.
-    """
-    return mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-
 class RamDumpDataset(Dataset):
-    def __init__(self, bin_path, meta_path, chunk_size=512, mode='binary'):
+    def __init__(self, bin_path, meta_path, chunk_size=512):
+        self.bin_path = bin_path
         self.chunk_size = chunk_size
         self.samples = []
-        
-        # 1. Charger les données
-        with open(bin_path, 'rb') as f:
-            self.ram_data = mmap_file(f)
+        self.ram_data = None
         
         with open(meta_path, 'r') as f:
             self.metadata = json.load(f)
             
-        # 1 = Chiffré, 0 = Clair
         self.label_map = {
-            "ENCRYPTED": 1,
-            "COMPRESSED": 1,
-            "BINARY_TEXT": 0,
-            "BINARY_IMAGE": 1,
-            "BINARY_OTHER": 0,
-            "BINARY_PDF": 1, # Les PDF sont souvent compressés on considère donc comme chiffrés
-            "BASE64": 0,
-            "DECODED": 0,
-            "SYSTEM": 0,
-            "NOISE": 0
+            "ENCRYPTED": 1, "COMPRESSED": 1, "BINARY_TEXT": 0,
+            "BINARY_IMAGE": 1, "BINARY_OTHER": 0, "BINARY_PDF": 1,
+            "BASE64": 0, "DECODED": 0, "SYSTEM": 0, "NOISE": 0
         }
-
         self._prepare_samples()
 
     def _prepare_samples(self):
-        print("Découpage des données en chunks...")
         for entry in self.metadata:
             label_str = entry['type']
-            
-            if label_str not in self.label_map:
-                continue
-                
+            if label_str not in self.label_map: continue
             label = self.label_map[label_str]
-            
-            start = entry['data_start']
-            end = entry['data_end']
-            
+            start, end = entry['data_start'], entry['data_end']
             current_pos = start
             while current_pos + self.chunk_size <= end:
                 self.samples.append((current_pos, label))
                 current_pos += self.chunk_size
 
-        print(f"Dataset prêt : {len(self.samples)} échantillons générés.")
-
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        offset, label = self.samples[idx]
+        if self.ram_data is None:
+            f = open(self.bin_path, 'rb')
+            self.ram_data = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
         
+        offset, label = self.samples[idx]
         chunk = self.ram_data[offset : offset + self.chunk_size]
         
         x = torch.tensor(list(chunk), dtype=torch.long)
