2fe03c608b82908a8efb965e4606653bc054b4dd
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2026-03-17 01:36:17 +0100

    fix(config) use real path with os module, regroup export of visualisation in output file

--- src/config.py
+++ src/config.py
@@ -1,3 +1,7 @@
-BIN_PATH = "output/ram_dump.bin"
-META_PATH = "output/metadata.json"
-MODEL_PATH = "bytes_transformer_classifier_all.pth"
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+BIN_PATH = os.path.join(BASE_DIR, "../output/ram_dump.bin")
+META_PATH = os.path.join(BASE_DIR, "../output/metadata.json")
+MODEL_PATH = os.path.join(BASE_DIR, "../bytes_transformer_classifier_all.pth")
+VISUAL_EXPORT_DIR = os.path.join(BASE_DIR, "../output")
